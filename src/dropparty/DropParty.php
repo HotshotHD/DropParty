85cdbe6dd9863808170fa7434ab231292ff02b3e
Kareem <hshot9930@gmail.com>
2016-01-05 12:54:35 -0500

    Update DropParty.php

--- src/dropparty/DropParty.php
+++ src/dropparty/DropParty.php
@@ -15,28 +15,28 @@ class DropParty extends PluginBase {
 	public $time;
 	
 	public function onEnable() {
-		$this->getLogger()->info("Has been enabled");
-		@mkdir($this->getDataFolder());
-		@mkdir($this->getDataFolder() . "config/");
-		$this->cfg = (new Config($this->getDataFolder() . "config/" . "config.yml", Config::YAML, array(
-		"World" => "world",
-		"Time" => 10,
-		"Coordinates" => [
-		"X" => 0,
-		"Y" => 0,
-		"Z" => 0,
-		],
-		"Items" => [
-		57,
-		42,
-		22,
-		41,
-		],
-		)))->getAll();
+          $this->getLogger()->info("Has been enabled");
+	  @mkdir($this->getDataFolder());
+	  @mkdir($this->getDataFolder() . "config/");
+	  $this->cfg = (new Config($this->getDataFolder() . "config/" . "config.yml", Config::YAML, array(
+	  "World" => "world",
+	  "Time" => 10,
+	  "Coordinates" => [
+	  "X" => 0,
+	  "Y" => 0,
+	  "Z" => 0,
+	  ],
+	  "Items" => [
+	  57,
+	  42,
+	  22,
+	  41,
+	  ],
+	  )))->getAll();
 		
-		$this->time = $this->cfg["Time"];
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new task\DropPartyTask($this), 20 * 60);
-		$this->getServer()->getScheduler()->scheduleRepeatingTask(new task\DropItemsTask($this), 20);
+	  $this->time = $this->cfg["Time"];
+	  $this->getServer()->getScheduler()->scheduleRepeatingTask(new task\DropPartyTask($this), 20 * 60);
+	  $this->getServer()->getScheduler()->scheduleRepeatingTask(new task\DropItemsTask($this), 20);
 	}
 	
 	public function getDropPartyTask() {
