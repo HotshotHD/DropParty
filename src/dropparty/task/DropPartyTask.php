bc4b436c2e5a18631a7c680c37fd33dfd6d1cbd9
Kareem <hshot9930@gmail.com>
2016-01-04 23:16:46 -0500

    Create DropPartyTask.php

---
+++ src/dropparty/task/DropPartyTask.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace dropparty\task;
+
+use pocketmine\scheduler\PluginTask;
+use dropparty\DropParty;
+use pocketmine\item\Item;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+
+class DropPartyTask extends PluginTask {
+	
+	public function __construct(DropParty $plugin) {
+		parent::__construct($plugin);
+		$this->plugin = $plugin;
+	}
+	
+	public function getPlugin() {
+		return $this->plugin;
+	}
+	
+	public function onRun($currentTick) {
+		$this->getPlugin()->time = $this->getPlugin()->time - 1;
+		
+		if($this->getPlugin()->time > 0) {
+			$this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7] §aStarting in §7" . $this->getPlugin()->time . " §amins.");
+		}
+		if($this->getPlugin()->time == 0) {
+			$this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7]§a Has started!");
+			$this->getPlugin()->status = "enabled";
+			
+		}
+			
+			
+		}
+		
+	}
