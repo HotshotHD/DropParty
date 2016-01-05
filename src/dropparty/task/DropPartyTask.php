c640d41024bb3b412db8f4d03053f144e4b5d8c1
Kareem <hshot9930@gmail.com>
2016-01-04 23:23:31 -0500

    Update DropPartyTask.php

--- src/dropparty/task/DropPartyTask.php
+++ src/dropparty/task/DropPartyTask.php
@@ -11,27 +11,27 @@ use pocketmine\math\Vector3;
 class DropPartyTask extends PluginTask {
 	
 	public function __construct(DropParty $plugin) {
-		parent::__construct($plugin);
-		$this->plugin = $plugin;
+	  parent::__construct($plugin);
+	  $this->plugin = $plugin;
 	}
 	
 	public function getPlugin() {
-		return $this->plugin;
+	  return $this->plugin;
 	}
 	
 	public function onRun($currentTick) {
-		$this->getPlugin()->time = $this->getPlugin()->time - 1;
+	  $this->getPlugin()->time = $this->getPlugin()->time - 1;
 		
-		if($this->getPlugin()->time > 0) {
-			$this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7] §aStarting in §7" . $this->getPlugin()->time . " §amins.");
-		}
-		if($this->getPlugin()->time == 0) {
-			$this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7]§a Has started!");
-			$this->getPlugin()->status = "enabled";
+	  if($this->getPlugin()->time > 0) {
+	    $this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7] §aStarting in §7" . $this->getPlugin()->time . " §amins.");
+	   }
+	  if($this->getPlugin()->time == 0) {
+	    $this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7]§a Has started!");
+	    $this->getPlugin()->status = "enabled";
 			
-		}
+	   }
 			
 			
-		}
-		
 	}
+		
+}
