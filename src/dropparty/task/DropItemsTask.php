a0bc19280d94b794fb1bbed3714c78776dd882b4
Kareem <hshot9930@gmail.com>
2016-01-04 23:17:13 -0500

    Create DropItemsTask.php

---
+++ src/dropparty/task/DropItemsTask.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace dropparty\task;
+
+use pocketmine\scheduler\PluginTask;
+use pocketmine\item\Item;
+use pocketmine\utils\Config;
+use pocketmine\math\Vector3;
+use dropparty\DropParty;
+
+class DropItemsTask extends PluginTask {
+	
+	public function __construct(DropParty $plugin) {
+		parent::__construct($plugin);
+		$this->plugin = $plugin;	
+	}
+	
+	public function getPlugin() {
+		return $this->plugin;
+	}
+	
+	public function onRun($currentTick) {
+		
+		if($this->getPlugin()->status == "enabled") {
+			$level = $this->getPlugin()->getServer()->getLevelByName($this->getPlugin()->cfg["World"]);
+			
+			foreach($this->getPlugin()->getServer()->getOnlinePlayers() as $p) {
+				$p->sendPopup("§7[§dDropParty§7] §aItems are dropping!");
+		    }
+			$this->getPlugin()->secs++;
+			
+			if($level !== null) {
+			$level->dropItem(new Vector3($this->getPlugin()->cfg["Coordinates"]["X"], $this->getPlugin()->cfg["Coordinates"]["Y"], $this->getPlugin()->cfg["Coordinates"]["Z"]), Item::get($this->getPlugin()->getRandomItem(), 0, mt_rand(1, 5)));
+			} else {
+				$this->getPlugin()->getLogger()->warning("§cItems could not be dropped. World doesn't exist.");
+			}
+		}
+		
+	    if($this->getPlugin()->secs == 60) {			
+			if($this->getPlugin()->status == "enabled") {
+				$this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7] §aHas ended!");
+				$this->getPlugin()->status = "ended";
+				$this->getPlugin()->secs = 0;				
+				$this->getPlugin()->time = $this->getPlugin()->cfg["Time"];
+			}
+			}
+	}
+		
+}
