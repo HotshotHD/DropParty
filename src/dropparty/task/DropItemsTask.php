2da57beb5ea10991725202f9d1ee4a97eda0fe73
Kareem <hshot9930@gmail.com>
2016-01-08 10:27:03 -0500

    Update DropItemsTask.php

--- src/dropparty/task/DropItemsTask.php
+++ src/dropparty/task/DropItemsTask.php
@@ -11,37 +11,40 @@ use dropparty\DropParty;
 class DropItemsTask extends PluginTask {
 	
 	public function __construct(DropParty $plugin) {
-	  parent::__construct($plugin);
-          $this->plugin = $plugin;	
+		parent::__construct($plugin);
+		$this->plugin = $plugin;	
 	}
 	
 	public function getPlugin() {
-	  return $this->plugin;
+		return $this->plugin;
 	}
 	
 	public function onRun($currentTick) {
-          if($this->getPlugin()->status == "enabled") {
-	    $level = $this->getPlugin()->getServer()->getLevelByName($this->getPlugin()->cfg["World"]);
-			
-	    foreach($this->getPlugin()->getServer()->getOnlinePlayers() as $p) {
-	      $p->sendPopup("§7[§dDropParty§7] §aItems are dropping!");
-            }
-            $this->getPlugin()->secs++;
-			
-	    if($level !== null) {
-	      $level->dropItem(new Vector3($this->getPlugin()->cfg["Coordinates"]["X"], $this->getPlugin()->cfg["Coordinates"]["Y"], $this->getPlugin()->cfg["Coordinates"]["Z"]), Item::get($this->getPlugin()->getRandomItem(), 0, mt_rand(1, 5)));
-	    } else {
-	        $this->getPlugin()->getLogger()->warning("§cItems could not be dropped. World doesn't exist.");
-	      }
-	    }
 		
-	    if($this->getPlugin()->secs == 60) {			
+		if($this->getPlugin()->status == "enabled") {
+		  $level = $this->getPlugin()->getServer()->getLevelByName($this->getPlugin()->cfg["World"]);
+			
+		  foreach($this->getPlugin()->getServer()->getOnlinePlayers() as $p) {
+		    if($this->getPlugin()->config()["Popup"] == true) {
+		      $p->sendPopup($this->getPlugin()->config()["Popup.Message"]);
+		     }
+		    }
+			$this->getPlugin()->secs++;
+			
+			if($level !== null) {
+			  $level->dropItem(new Vector3($this->getPlugin()->cfg["Coordinates"]["X"], $this->getPlugin()->cfg["Coordinates"]["Y"], $this->getPlugin()->cfg["Coordinates"]["Z"]), Item::get($this->getPlugin()->getRandomItem(), 0, mt_rand(1, 5)));
+			} else {
+			  $this->getPlugin()->getLogger()->warning("§cItems could not be dropped. World doesn't exist.");
+			}
+		}
+		
+	    if($this->getPlugin()->secs == $this->getPlugin()->config()["Duration"]) {			
 	      if($this->getPlugin()->status == "enabled") {
-	        $this->getPlugin()->getServer()->broadcastMessage("§7[§dDropParty§7] §aHas ended!");
+		$this->getPlugin()->getServer()->broadcastMessage($this->getPlugin()->config()["Message.Ended"]);
 		$this->getPlugin()->status = "ended";
 		$this->getPlugin()->secs = 0;				
 		$this->getPlugin()->time = $this->getPlugin()->cfg["Time"];
-	      }
+	       }
 	    }
 	}
 		
